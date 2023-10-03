ea982d82ab2d3665c473c79d59e713d9b8e8d3c9
Puria Nafisi Azizi <puria@dyne.org>
2023-10-03 19:02:13 +0200

    Initial commit :tada:

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+const konstaConfig = require('konsta/config');
+
+module.exports = konstaConfig({
+  content: ['./src/**/*.{js,jsx,ts,tsx,svelte}', './src/index.html'],
+  darkMode: 'class',
+});
+
+
